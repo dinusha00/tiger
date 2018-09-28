a7345550275634d65cd5266701c7adce39772ca8
Dinusha Ambagahawita <dinusha.ambagahawita@rakuten.com>
2018-09-28 09:53:56 +0800

    test

--- src/main/scala/com/tiger/Main.scala
+++ src/main/scala/com/tiger/Main.scala
@@ -1,13 +1,14 @@
 package com.tiger
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.input_file_name
 
 object Main {
 
   val APP = "tiger"
 
   def main(args: Array[String]): Unit = {
-    println(s"calling $APP main")
+    println(s"calling $APP main v3")
 
     val dataPath = args(0)
     println(s"dataPath:$dataPath")
@@ -17,14 +18,16 @@ object Main {
 
     val spark = SparkSession.builder().appName(APP).getOrCreate()
 
-    val rdd = spark.sparkContext.wholeTextFiles(dataPath)
-    rdd.foreachPartition(partition => {
+    import spark.implicits._
+    val ds = spark.read.textFile(dataPath).select(input_file_name, $"value").as[(String, String)].rdd
+    ds.foreachPartition(partition => {
       partition.foreach(t => {
-        val name = t._1
+        val file = t._1
         val content = t._2
-        if (content.indexOf(keyToSearch) > 0) {
+        if (content != null && content.indexOf(keyToSearch) > 0) {
           println("#########################################################")
-          println(name)
+          println(content)
+          println(file)
           println("#########################################################")
         }
       })
